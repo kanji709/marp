97d0e8383d066ed7356c314702d747afdac24523
Jie Kang <70739077+kanji709@users.noreply.github.com>
2021-10-15 08:47:10 +1300

    Update test-pllog.R
    change tolerance level in all.equal() from 1.5e-8 to 1.5e-7

--- tests/testthat/test-pllog.R
+++ tests/testthat/test-pllog.R
@@ -5,7 +5,7 @@ test_that("pllog", {
   log <- FALSE
   result <- pllog(q, shape, scale, log)
   expected_result <- as.numeric(c(0.9959016, 0.8836364, 0.5000000, 0.1917916))
-  expect_true(all.equal(result, expected_result))
+  expect_true(all.equal(result, expected_result, tolerance = 1e-7))
   
   q <- c(1,2,3,4)
   shape <- 5
@@ -13,5 +13,5 @@ test_that("pllog", {
   log <- TRUE
   result <- pllog(q, shape, scale, log)
   expected_result <- as.numeric(c(0.004098361, 0.116363636, 0.500000000, 0.808208366))
-  expect_true(all.equal(result, expected_result))
+  expect_true(all.equal(result, expected_result, tolerance = 1e-7))
 })
