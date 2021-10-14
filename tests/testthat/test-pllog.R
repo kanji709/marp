7bf1627b6298f756b6b2085a4359c3eb82d4fc02
Jie Kang <70739077+kanji709@users.noreply.github.com>
2021-10-15 12:03:46 +1300

    Update test-pllog.R
    undo tolerance level change

--- tests/testthat/test-pllog.R
+++ tests/testthat/test-pllog.R
@@ -1,17 +1,17 @@
 test_that("pllog", {
-  q <- c(1,2,3,4)
+  q <- c(1, 2, 3, 4)
   shape <- 5
   scale <- 3
   log <- FALSE
   result <- pllog(q, shape, scale, log)
-  expected_result <- as.numeric(c(0.9959016, 0.8836364, 0.5000000, 0.1917916))
-  expect_true(all.equal(result, expected_result, tolerance = 1e-7))
+  expected_result <- as.numeric(c(0.995901639344262 , 0.883636363636364 , 0.500000000000000 , 0.191791633780584))
+  expect_true(all.equal(result, expected_result))
   
-  q <- c(1,2,3,4)
+  q <- c(1, 2, 3, 4)
   shape <- 5
   scale <- 3
   log <- TRUE
   result <- pllog(q, shape, scale, log)
-  expected_result <- as.numeric(c(0.004098361, 0.116363636, 0.500000000, 0.808208366))
-  expect_true(all.equal(result, expected_result, tolerance = 1e-7))
+  expected_result <-  as.numeric(c(0.0040983606557377 , 0.1163636363636363 , 0.5000000000000000 , 0.8082083662194159))
+  expect_true(all.equal(result, expected_result, tolerance = 1.5e-7))
 })
