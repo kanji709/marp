c4888ba292f335c26f2bce1df75d0dfffe25a3e9
Vanessa Huang <vaness51139@gmail.com>
2026-01-02 11:21:47 +1100

    discard invalid bootstrap repls for all estimates

--- R/student_confint.R
+++ R/student_confint.R
@@ -112,25 +112,25 @@ student_confint <- function(n,B,t,m,BB,par_hat,mu_hat,pr_hat,haz_hat,weights,alp
   ## using all six renewal models
   ## find model-averaged studentized bootstrap CIs of estimated mean, (logit) probability and (log) hazard rates
   mu_lower_ma <- stats::uniroot(function(low) lowerT(low, mu_hat, mu_var_hat, mu_Tstar, weights, B, alpha),
-                         lower = min(sapply(1:6, function(i) mu_hat[i] - max(mu_Tstar[i,]) * sqrt(mu_var_hat[i]))),
-                         upper = max(sapply(1:6, function(i) mu_hat[i] - stats::quantile(mu_Tstar[i,], prob = 0.9) * sqrt(mu_var_hat[i]))))$root
+                         lower = min(sapply(1:6, function(i) mu_hat[i] - max(mu_Tstar[i,], na.rm = TRUE) * sqrt(mu_var_hat[i])), na.rm = TRUE),
+                         upper = max(sapply(1:6, function(i) mu_hat[i] - stats::quantile(mu_Tstar[i,], prob = 0.9, na.rm=TRUE) * sqrt(mu_var_hat[i])), na.rm = TRUE))$root
   mu_upper_ma <- stats::uniroot(function(up) upperT(up, mu_hat, mu_var_hat, mu_Tstar, weights, B, alpha),
-                         lower = min(sapply(1:6, function(i) mu_hat[i] - stats::quantile(mu_Tstar[i,], prob = 0.1) * sqrt(mu_var_hat[i]))),
-                         upper = max(sapply(1:6, function(i) mu_hat[i] - min(mu_Tstar[i,]) * sqrt(mu_var_hat[i]))))$root
+                         lower = min(sapply(1:6, function(i) mu_hat[i] - stats::quantile(mu_Tstar[i,], prob = 0.1, na.rm=TRUE) * sqrt(mu_var_hat[i])), na.rm=TRUE),
+                         upper = max(sapply(1:6, function(i) mu_hat[i] - min(mu_Tstar[i,], na.rm=TRUE) * sqrt(mu_var_hat[i])), na.rm=TRUE))$root
   pr_lower_ma <- stats::uniroot(function(low) lowerT(low, pr_hat, pr_var_hat, pr_Tstar, weights, B, alpha),
-                         lower = min(sapply(1:6, function(i) pr_hat[i] - max(pr_Tstar[i,]) * sqrt(pr_var_hat[i]))),
-                         upper = max(sapply(1:6, function(i) pr_hat[i] - stats::quantile(pr_Tstar[i,], prob = 0.9, na.rm=TRUE) * sqrt(pr_var_hat[i]))))$root
+                         lower = min(sapply(1:6, function(i) pr_hat[i] - max(pr_Tstar[i,], na.rm=TRUE) * sqrt(pr_var_hat[i])), na.rm=TRUE),
+                         upper = max(sapply(1:6, function(i) pr_hat[i] - stats::quantile(pr_Tstar[i,], prob = 0.9, na.rm=TRUE) * sqrt(pr_var_hat[i])), na.rm=TRUE))$root
   pr_upper_ma <- stats::uniroot(function(up) upperT(up, pr_hat, pr_var_hat, pr_Tstar, weights, B, alpha),
-                         lower =  min(sapply(1:6, function(i) pr_hat[i] - stats::quantile(pr_Tstar[i,], prob = 0.1) * sqrt(pr_var_hat[i]))),
-                         upper = max(sapply(1:6, function(i) pr_hat[i] - min(pr_Tstar[i,]) * sqrt(pr_var_hat[i]))))$root
+                         lower =  min(sapply(1:6, function(i) pr_hat[i] - stats::quantile(pr_Tstar[i,], prob = 0.1, na.rm=TRUE) * sqrt(pr_var_hat[i])), na.rm=TRUE),
+                         upper = max(sapply(1:6, function(i) pr_hat[i] - min(pr_Tstar[i,], na.rm=TRUE) * sqrt(pr_var_hat[i])), na.rm=TRUE))$root
   haz_lower_ma <- sapply(1:length(t), function(i)
     stats::uniroot(function(low) lowerT(low, haz_hat[i, ], haz_var_hat[i, , 1], haz_Tstar[i, ,], weights, B, alpha),
-            lower = min(sapply(1:6, function(j) haz_hat[i, j] - max(haz_Tstar[i, j,]) * sqrt(haz_var_hat[i, j, 1]))),
-            upper = max(sapply(1:6, function(j) haz_hat[i, j] - stats::quantile(haz_Tstar[i, j,], prob = 0.9) * sqrt(haz_var_hat[i, j, 1]))))$root)
+            lower = min(sapply(1:6, function(j) haz_hat[i, j] - max(haz_Tstar[i, j,], na.rm=TRUE) * sqrt(haz_var_hat[i, j, 1])), na.rm=TRUE),
+            upper = max(sapply(1:6, function(j) haz_hat[i, j] - stats::quantile(haz_Tstar[i, j,], prob = 0.9, na.rm=TRUE) * sqrt(haz_var_hat[i, j, 1])), na.rm=TRUE))$root)
   haz_upper_ma <- sapply(1:length(t), function(i)
     stats::uniroot(function(up) upperT(up, haz_hat[i, ], haz_var_hat[i, , 1], haz_Tstar[i, ,], weights, B, alpha),
-            lower = min(sapply(1:6, function(j) haz_hat[i, j] - stats::quantile(haz_Tstar[i, j,], prob = 0.1) * sqrt(haz_var_hat[i, j, 1]))),
-            upper = max(sapply(1:6, function(j) haz_hat[i, j] - min(haz_Tstar[i, j,]) * sqrt(haz_var_hat[i, j, 1]))))$root)
+            lower = min(sapply(1:6, function(j) haz_hat[i, j] - stats::quantile(haz_Tstar[i, j,], prob = 0.1, na.rm=TRUE) * sqrt(haz_var_hat[i, j, 1])), na.rm=TRUE),
+            upper = max(sapply(1:6, function(j) haz_hat[i, j] - min(haz_Tstar[i, j,], na.rm=TRUE) * sqrt(haz_var_hat[i, j, 1])), na.rm=TRUE))$root)
   return(list("mu_lower_gen" = unname(mu_lower_gen),
               "mu_upper_gen" = unname(mu_upper_gen),
               "pr_lower_gen" = unname(pr_lower_gen),
